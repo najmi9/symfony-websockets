6e1c103680669b8392c45a3e331ddd5de703ee44
najmi9 <imadnajmi9@gmail.com>
2022-10-25 23:18:40 +0100

    use switch case

--- assets/js/App.jsx
+++ assets/js/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ConnectedUsers from "./components/ConnectedUsers";
 import {user} from './user';
 import Header from "./components/Header";
@@ -10,9 +10,9 @@ import { WS_URL } from "../config";
 export default function App() {
     const ws = new WebSocket(WS_URL);
 
-    const JOIN = 'USER_JOINED';
-    const MSG = 'NEW_MESSAGE';
-    const CONV = 'NEW_CONVERSATION';
+    const USER_JOINED = 'USER_JOINED';
+    const NEW_MESSAGE = 'NEW_MESSAGE';
+    const NEW_CONVERSATION = 'NEW_CONVERSATION';
 
     const [connectedUsers, setConnectedUsers] = useState([]);
 
@@ -34,13 +34,12 @@ export default function App() {
 
         setCurrentConversation(c => ({...conversation, ...c}));
 
-        conversation.type = CONV;
+        conversation.type = NEW_CONVERSATION;
         ws.send(JSON.stringify(conversation));
     }
 
-    const onUserCloseConversation = () => {
-        setState('IDLE')  
-    }
+    const onUserCloseConversation = () => setState('IDLE');
+
     const onCreatorSubmitMessage = e => {
         const form = Object.fromEntries(new FormData(e.target));
 
@@ -48,7 +47,7 @@ export default function App() {
             user: user,
             to: chatWith,
             msg: form.msg,
-            type: MSG,
+            type: NEW_MESSAGE,
         };
 
         ws.send(JSON.stringify(sendMessage));
@@ -58,48 +57,41 @@ export default function App() {
         setCurrentConversation(c => ({...c, msgs: [...c.msgs, sendMessage]}));
     }
 
-    useEffect(() => {
-        ws.onopen = function () {
-            ws.send(JSON.stringify({user: user, type: JOIN}));
-        };
+    ws.onopen = () => ws.send(JSON.stringify({user: user, type: USER_JOINED}));
 
-        ws.onmessage = function (e) {
-            const data = JSON.parse(e.data);
-            const isPush = JOIN === data.type && data.user.id !== user.id;
-            const isUserAlreadyJoined = connectedUsers.filter(u => u.user.id === data.user.id).length > 0;
+    ws.onmessage = e => {
+        const data = JSON.parse(e.data);
 
-            if (isPush && !isUserAlreadyJoined) {
-                setConnectedUsers(cu => [...cu, data.user]);
-                return
-            }
-            
-            if (CONV === data.type) {
-                const isUserExists = connectedUsers.filter(cu => cu.id === data.creator.id);
-                
-                if (0 === isUserExists.length) {
-                    setConnectedUsers(cu =>[data.creator, ...cu]);
+        switch (data.type) {
+            case USER_JOINED:
+                const isUserAlreadyJoined = connectedUsers.filter(u => u.id === data.user.id).length;
+                if (data.user.id !== user.id && 0 === isUserAlreadyJoined) {
+                    setConnectedUsers(cu => [...cu, data.user]);
                 }
-    
+                break;
+            case NEW_CONVERSATION:
+                const isUserExists = connectedUsers.filter(cu => cu.id === data.creator.id);
+            
+                if (0 === isUserExists.length) {
+                    setConnectedUsers(cu => [data.creator, ...cu]);
+                }
+
                 notify(
                     'New chat',
                     `${data.creator.username} start a conversation`,
                     data.creator.avatar
                 );
-              
-                return;
-            }
-            
-            if (MSG === data.type) {
+                break;
+            case NEW_MESSAGE:
                 setCurrentConversation(c => ({...c, msgs: [...(c.msgs || []), data]}));
                 notify(
                     `${data.user.username} send you a message`,
                     data.msg,
                     data.user.avatar
                 );
-                return;
-            }
-        };
-    }, []);
+                break;
+        }
+    };
 
     return <div className="card bg-dark mt-4">
         <Header user={user} />
